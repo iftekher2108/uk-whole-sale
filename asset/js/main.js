e367138209759a40e8773caffa21c467d47849e6
iftekher2108 <parvezkhan21082002@gmail.com>
2024-04-20 14:05:33 +0600

    mejor ui and responsive fixed

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,23 @@
+$(document).ready(function () {
+
+  // scroll functionality
+  $(document).scroll(function () {
+    if ($(document).scrollTop() > 10) {
+      $('.top-banner').slideUp('fast');
+    } else {
+      $('.top-banner').slideDown('fast');
+    }
+  })
+
 
 // slick slider 
-
 $('.product-slider').slick({
   arrows: true,
   slidesToShow: 4,
   slidesToScroll: 4,
   dots: true,
   infinite: false,
-  responsive: [
-    {
+  responsive: [{
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
@@ -48,16 +57,28 @@ $('.hero-slider').slick({
 });
 // slick slider
 
-
 // hero slide arrow  css
-$(".hero-slider .slick-prev").css({"top":"47%","left":"3%","z-index":"30"});
-$(".hero-slider .slick-next").css({"top":"47%","right":" 5.5%"});
+$(".hero-slider .slick-prev").css({
+  "top": "47%",
+  "left": "3%",
+  "z-index": "30"
+});
+$(".hero-slider .slick-next").css({
+  "top": "47%",
+  "right": " 5.5%"
+});
 // hero slide arrow css
 
 
 // catagory hide show
-$(".main-cat-btn").click(function(){
-  $(".menu").toggle(400)
+$(".main-cat-btn").click(function () {
+$(".menu").toggle(400)
 })
-
 // catagory hide show
+
+
+
+
+
+
+})
