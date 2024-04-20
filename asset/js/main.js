702a786a042b79726fcd020cf1b7fb787b6d2295
iftekher2108 <parvezkhan21082002@gmail.com>
2024-04-20 16:56:12 +0600

    mejor bug fix

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+  $('#loader').fadeOut('slow');
+  
   // scroll functionality
   $(document).scroll(function () {
     if ($(document).scrollTop() > 10) {
@@ -45,6 +47,44 @@ $('.product-slider').slick({
     // instead of a settings object
   ]
 });
+
+$('.brand-slider').slick({
+  arrows: true,
+  slidesToShow: 7,
+  slidesToScroll: 4,
+  dots: true,
+  infinite: true,
+  autoPlay: true,
+  autoplaySpeed: 1500,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 
 $('.hero-slider').slick({
   arrows: true,
